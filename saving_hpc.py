3ffa8852bd56d33a93d41a0d7e93249801b99e00
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-05-26 12:32:59 +0100

    Update saving_hpc.py

--- saving_hpc.py
+++ saving_hpc.py
@@ -7,3 +7,8 @@ np.save(filename_gt, rv_es)
  print(out_LA_ES_RV.shape)
  filepath = os.path.join(target_dir1, str(batch_idx)+'_pre_ES_RV.png')
  torchvision.utils.save_image(out_LA_ES_RV[0,0,:,:], filepath)
+
+              ###  saving the gts  
+to_format_img = nib.Nifti1Image(gt_single, np.eye(4))  
+to_format_img.set_data_dtype(np.uint8)
+to_format_img.to_filename(os.path.join(pre_apth,name[0]+'_'+str(i)+'_gt'+'.nii.gz'))
