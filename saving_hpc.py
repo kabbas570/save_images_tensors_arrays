c054a5e460103be375cc92ef0d1b3c6caa31a9b2
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-05-30 21:52:38 +0100

    Update saving_hpc.py

--- saving_hpc.py
+++ saving_hpc.py
@@ -15,3 +15,18 @@ to_format_img.to_filename(os.path.join(pre_apth,name[0]+'_'+str(i)+'_gt'+'.nii.g
 
 
  #plt.imsave('D:\\Greg\\Research\\Code\\abbas\\001\\001_Orig_sa' + str(i) + '.png', temp[i, :, :])
+            
+                                    #### Saving the Visual FM and Reuslts  #####
+            name = name[0]
+            out_LA_ES_LV = out_LA_ES_LV[0,0,:].float()
+            out_LA_ES_MYO = out_LA_ES_MYO[0,0,:].float()
+            out_LA_ES_RV = out_LA_ES_RV[0,0,:].float()
+
+            filepath = os.path.join(target_dir1, str(name)+'_LV'+'.png')
+            torchvision.utils.save_image(out_LA_ES_LV, filepath)
+            
+            filepath = os.path.join(target_dir1, str(name)+'_MYO'+'.png')
+            torchvision.utils.save_image(out_LA_ES_MYO, filepath)
+            
+            filepath = os.path.join(target_dir1, str(name)+'_RV'+'.png')
+            torchvision.utils.save_image(out_LA_ES_RV, filepath)
