551830e760295ebbac62c6e4621b24d95636532a
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-05-31 16:06:04 +0100

    Update saving_hpc.py

--- saving_hpc.py
+++ saving_hpc.py
@@ -8,6 +8,9 @@ np.save(filename_gt, rv_es)
  filepath = os.path.join(target_dir1, str(batch_idx)+'_pre_ES_RV.png')
  torchvision.utils.save_image(out_LA_ES_RV[0,0,:,:], filepath)
 
+import nibabel as nib
+import os
+
               ###  saving the gts  
 to_format_img = nib.Nifti1Image(gt_single, np.eye(4))  
 to_format_img.set_data_dtype(np.uint8)
