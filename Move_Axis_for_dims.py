7f6b5bd994786e28491cd832a4b805410998c04a
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-06-05 09:33:04 +0100

    Update Move_Axis_for_dims.py

--- Move_Axis_for_dims.py
+++ Move_Axis_for_dims.py
@@ -2,5 +2,8 @@ import numpy as np
 a1 = np.zeros([55,6,10])
 print(a1.shape)
 
-a = np.moveaxis(a1,-1,0)  ### move last dimenssion to first 
-print(a.shape)
+a2 = np.moveaxis(a1,-1,0)  ### move last dimenssion to first 
+print(a2.shape)
+
+a3 = np.moveaxis(a1,0,-1)  ### move first dimenssion to last 
+print(a3.shape)
