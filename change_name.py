f8d004fe3fd37d96b1052baeccbd86078d531d7d
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-07-28 00:29:22 +0100

    Update change_name.py

--- change_name.py
+++ change_name.py
@@ -5,12 +5,12 @@ import numpy as np
 import glob
 
 prob_id_orig = []
-for infile in sorted(glob.glob(r"C:\Users\Abbas Khan\Downloads\nii_gz/*.nii.gz")): 
+for infile in sorted(glob.glob("/data/scratch/acw676/inp_imgs/org/*.nii.gz")): 
     prob_id_orig.append(infile)
     
 
-save_path = r'C:\Users\Abbas Khan\Downloads\four/'
-temp = r"C:\Users\Abbas Khan\Downloads\nii_gz/"
+save_path = '/data/scratch/acw676/inp_imgs/org/'
+temp = "/data/scratch/acw676/inp_imgs/org/all_nn/"
 
 for i in range(4):
     img = sitk.ReadImage(prob_id_orig[i])
